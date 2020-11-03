42ab5f0a8dc3a5521857d66150aefcccf61b8a67
joshua londono <joshualondono@outlook.com>
2020-11-03 16:35:58 -0500

    server is owrking response

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,41 +1,6 @@
-const express = require('express'); //create a router
-const router = express.Router(); //users array
-let data = require('../models/dataArray');
-const Data = require('../models/Schema'); // get all users
+const express = require('express'); 
+const router = express.Router();
+const {response} = require('../controllers/appController')
 
-const {getAllSuggestions, byNameSuggestions, singleSuggestion} = require('../controllers/appController')
-
-router.get('/all-suggestions', getAllSuggestions);
-router.get('/byname-suggestions', byNameSuggestions);
-router.get('/single-suggestion', singleSuggestion);
-
-// router.get('/all-users', (req, res) => {
-
-// Data.find()
-//   .then((users) => {
-//     return res.status(200).json({ confirmation: 'success', data });
-//   }).catch(err=>res.status(500).json({confirmation: 'fail', err}));
-
-// });
-
-// router.get('/all-users', (req, res) => {
-
-// Data.find()
-//     .then((users) => {
-//       return res.status(200).json({ confirmation: 'success', data });
-//     }).catch(err=>res.status(500).json({confirmation: 'fail', err}));
-  
-//   });
-
-//   router.get('/all-users', (req, res) => {
-
-// Data.find()
-//     .then((users) => {
-//       return res.status(200).json({ confirmation: 'success', data });
-//     }).catch(err=>res.status(500).json({confirmation: 'fail', err}));
-  
-//   });
-
-
-
+router.get('/response', response);
 module.exports = router;
